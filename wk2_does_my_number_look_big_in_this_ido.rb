7b7742e70dff83cb237c97882f5604d9deefc12f
Ido Moskovich <imidom@gmail.com>
2019-06-05 16:57:27 -0400

    Renamed sum variable to something more descriptive

--- wk2_does_my_number_look_big_in_this_ido.rb
+++ wk2_does_my_number_look_big_in_this_ido.rb
@@ -1,9 +1,9 @@
 def narcissistic?(value)
   value_length = value.to_s.length
-  
-  sum_of_digits = value.digits
+
+  sum_of_powered_digits = value.digits
     .map{ |digit| digit ** value_length }
     .reduce(&:+)
-    
-  value == sum_of_digits
+
+  value == sum_of_powered_digits
 end
