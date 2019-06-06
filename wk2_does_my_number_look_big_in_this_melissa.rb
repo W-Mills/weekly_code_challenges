ed6880c3c9294453ae502599aacecbea7cde8534
Melissa Manousos <mmanousos@users.noreply.github.com>
2019-06-06 10:07:03 -0700

    Pushed before realizing there were now folders

--- wk2_does_my_number_look_big_in_this_melissa.rb
+++
@@ -1,8 +0,0 @@
-def narcissistic?(value)
-  digits = value.digits
-  digits.map { |digit| digit ** digits.size }.sum == value
-end
-
-puts narcissistic?(153)
-puts narcissistic?(1634)
-puts narcissistic?(1635)
