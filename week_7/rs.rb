bcb2037fe1bc08d408bb05783318227219bb8516
Ryan Schaul <rschaul@gmail.com>
2019-07-11 14:41:09 -0500

    add faster solution using a set as data structure

--- week_7/rs.rb
+++ week_7/rs.rb
@@ -1,23 +1,24 @@
 require 'pry'
+require 'set'
 # Part 1 - working solution
 
 input = File.read('./day1_raw.txt')
 
 directions = input.split("\n").map { |direction| direction.to_i }
 
-def frequency_finder(array)
-  sum = 0
+# def frequency_finder(array)
+#   sum = 0
 
-  array.each { |direction| sum += direction }
+#   array.each { |direction| sum += direction }
 
-  sum
-end
+#   sum
+# end
 
-p frequency_finder(directions) == 500
-p frequency_finder([1, 1, 1]) == 3
-p frequency_finder([1, 1, -2]) == 0
-p frequency_finder([-1, -2, -3]) == -6
-p frequency_finder([1, -2, 3, 1]) == 3
+# p frequency_finder(directions) == 500
+# p frequency_finder([1, 1, 1]) == 3
+# p frequency_finder([1, 1, -2]) == 0
+# p frequency_finder([-1, -2, -3]) == -6
+# p frequency_finder([1, -2, 3, 1]) == 3
 
 
 #Part 2 - working solution but too slow
@@ -31,7 +32,6 @@ p frequency_finder([1, -2, 3, 1]) == 3
 #     array.each do |direction| 
 #       sum += direction
 #       frequencies << sum
-#       # binding.pry
 #       break if frequencies.count(sum) == 2
 #     end
 #     count += 1
@@ -41,35 +41,30 @@ p frequency_finder([1, -2, 3, 1]) == 3
 #   frequencies[-1]
 # end
 
-# Part 2 - Trying to get a faster working solution. I don't understand how binary search works.
+# Part 2a - I got it to run fast; I used a set instead of an array to keep track of the frequencies I've already seen. I'm still curious about how to optimize the use of an array and if it's possible.
 
-# def frequency_finder(array)
-#   sum = 0
-#   frequencies = [0]
-#   count = 0
-#   found = false
+def frequency_finder(array)
+  sum = 0
+  frequencies = Set[0]
+  found = false
 
-#   loop do
-#     array.each do |direction| 
-#       sum += direction
-#       # binding.pry
-#       if frequencies.bsearch { |x| x == sum }
-#         found = true
-#         break
-#       end
-#       frequencies << sum
-#     end
-#     count += 1
-#     break if found
-#   end
+  loop do
+    array.each do |direction| 
+      sum += direction
+      if frequencies.include?(sum)
+        found = true
+        break
+      end
+      frequencies << sum
+    end
+    break if found
+  end
 
-#   sum
-# end
+  sum
+end
 
-# p frequency_finder([1, -1]) #== 0
-# p frequency_finder([3, 3, 4, -2, -4]) == 10
-# p frequency_finder([-6, 3, 8, 5, -6]) #== 5
-# p frequency_finder([7, 7, -2, -7, -4]) == 14
-# # p frequency_finder(directions)
-
-
+p frequency_finder([1, -1]) #== 0
+p frequency_finder([3, 3, 4, -2, -4]) #== 10
+p frequency_finder([-6, 3, 8, 5, -6]) #== 5
+p frequency_finder([7, 7, -2, -7, -4]) #== 14
+p frequency_finder(directions)
