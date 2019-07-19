e52af28280ac472d8171fae50f4f83046f3f00b5
Ido Moskovich <imidom@gmail.com>
2019-07-18 20:41:06 -0400

    WK7 Ido - Use Hash instead of Array

--- week_7/wk7_ido.rb
+++ week_7/wk7_ido.rb
@@ -20,73 +20,19 @@ puts final_frequency(device_inputs)
 
 ## PART 2
 
-## brute force
+## so much faster than an array
 def sum_until_duplicate(array)
-  sums = []
+  sums = {}
   sum = 0
 
   loop do
     array.each do |element|
       sum += element
-      return sum if sums.include?(sum)
+      return sum if sums.key?(sum)
 
-      sums << sum
+      sums[sum] = nil
     end
   end
 end
 
-# puts sum_until_duplicate(device_inputs.map(&:to_i))
-
-## second attempt
-def frequency_pairs(device_inputs)
-  frequencies = frequency_changes(device_inputs)
-  final_frequency = final_frequency(device_inputs)
-
-  pairs = []
-  distance = 1
-
-  loop do
-    first_index = 0
-    loop do
-      first_element = frequencies[first_index]
-      second_index = first_index + distance
-      second_element = frequencies[second_index]
-      range = second_element - first_element
-
-      if (range % final_frequency).zero?
-        if (range / final_frequency).positive?
-          index_to_save = first_index
-          sum_to_save = second_element
-        else
-          index_to_save = second_index
-          sum_to_save = first_element
-        end
-
-        pairs << {
-          index: index_to_save,
-          sum: sum_to_save,
-          distance: range.abs
-        }
-      end
-
-      first_index += 1
-      break if first_index >= frequencies.length - distance
-    end
-
-    distance += 1
-    break if distance >= frequencies.length
-  end
-
-  pairs
-end
-
-def first_repeated_sum(pairs)
-  first_pair = pairs.min_by do |pair|
-    [pair[:distance], pair[:index]]
-  end
-
-  first_pair[:sum]
-end
-
-pairs = frequency_pairs(device_inputs)
-p first_repeated_sum(pairs)
+puts sum_until_duplicate(device_inputs.map(&:to_i))
