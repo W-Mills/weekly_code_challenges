c23cb4a85b90453fa6956c8e273317441edbb4f1
Ido Moskovich <imidom@gmail.com>
2019-07-24 01:41:01 -0400

    Ido Week8 AdvDay2P2

--- week_8/wk8_ido.rb
+++ week_8/wk8_ido.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+## PART 1
 #   P
 #     Input:
 #       - a text file comprised of strings separated
@@ -21,7 +22,7 @@
 #   D
 #     We can store the box ids from the file as a string
 #     of arrays.
-#   C
+#   A
 #     1. Convert the data in the input file into an array (ids)
 #        of strings (box_ids).
 #     2. Initialize two integer variables, double & triple, to zero
@@ -29,6 +30,7 @@
 #         Increment double if id has two of the same character
 #         Increment triple if id has three of the same character
 #     4. Multiply double & triple.
+#   C
 
 def convert_input_to_array(file_path)
   input_data = File.read(file_path)
@@ -59,4 +61,39 @@ end
 FILE_PATH = './wk8_ido_input.txt'
 box_ids = convert_input_to_array(FILE_PATH)
 
-puts calculate_checksum(box_ids)
+puts "Checksum: #{calculate_checksum(box_ids)}"
+
+## PART 2
+## My algorithm overcomplicated my solution,
+## so I did went a different route from my
+## original PEDAC.
+
+def common_chars_if_off_by_1(id1, id2)
+  common_chars = ''
+  discrepancy_count = 0
+
+  id1.chars.each_with_index do |char, index|
+    if char == id2[index]
+      common_chars += char
+    else
+      discrepancy_count += 1
+    end
+
+    return false if discrepancy_count > 1
+  end
+
+  discrepancy_count == 1 ? common_chars : false
+end
+
+def get_common_chars(box_ids)
+  until box_ids.empty?
+    id1 = box_ids.pop
+
+    box_ids.each do |id2|
+      common_chars = common_chars_if_off_by_1(id1, id2)
+      return common_chars if common_chars
+    end
+  end
+end
+
+p get_common_chars(box_ids)
