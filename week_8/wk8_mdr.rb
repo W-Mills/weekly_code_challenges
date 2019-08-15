78661c91eff63b12529559ee2b09f4372e84a950
Mike del Rio <mikevdelrio@gmail.com>
2019-08-15 10:32:26 -0400

    added partII

--- week_8/wk8_mdr.rb
+++ week_8/wk8_mdr.rb
@@ -1,6 +1,8 @@
 box_ids = File.read("wk8_mdr_input.txt")
 box_ids = box_ids.split(/\s/).map(&:chars)
 
+# part one
+
 total_repeats = {pairs: 0, triplets: 0}
 
 box_ids.each do |box_id|
@@ -13,3 +15,41 @@ box_ids.each do |box_id|
 end
 
 total_repeats[:pairs] * total_repeats[:triplets]
+
+# part two
+
+def find_similar_ids(box_ids)
+  similar_ids = []
+
+  box_ids.each_with_index do |id, index|
+    box_ids[(index + 1)...].each do |id2|
+      number_errors = 0
+      id.each_with_index do |character, index_inner|
+        if character != id2[index_inner]
+          number_errors += 1
+        end
+      end
+      if number_errors <= 1
+        similar_ids.push([id, id2])
+      end
+    end
+  end
+
+  similar_ids
+end
+
+def matching_id_characters(pair_ids)
+  pair_ids.flatten!(1)
+  common_characters = []
+
+  pair_ids[0].each_with_index do |character, index|
+    if character == pair_ids[1][index]
+      common_characters.push(character)
+    end
+  end
+
+  common_characters.join
+end
+
+pair_ids = find_similar_ids(box_ids)
+matching_id_characters(pair_ids)
